875114aa1e1187db1d1ec8fe0c22dc5509dd3535
Christophe Grand <christophe@cgrand.net>
2014-01-14 15:35:18 +0100

    css-selectors merged into html ns

--- src/enliven/html.clj
+++ src/enliven/html.clj
@@ -6,8 +6,7 @@
     [enliven.core.grounder :as grounder]
     [enliven.core.plans :as plan]
     [enliven.html.emit.static :as static]
-    [enliven.commons.emit.static :refer [render tighten]]
-    [enliven.html.css-selectors :as css]
+    [enliven.commons.emit.static :as common]
     [clojure.string :as str]))
 
 ;; html-specific segments
@@ -17,11 +16,107 @@
   :putback 
     (some->> classes (keep (fn [[k v]] (when v k))) seq (str/join " ")))
 
-(defn sel [selector]
-  (cond
-    (string? selector) (css/css selector)
-    (keyword? selector) (css/css (name selector))
-    :else selector))
+(defn children [loc]
+  (when (:tag (loc/node loc))
+    (let [loc (loc/down loc :content)]
+      (for [i (range (count (loc/node loc)))]
+        (loc/down loc i)))))
+
+(defn text-node [loc]
+  (when (string? (loc/node loc)) (list loc)))
+
+(defn descendants [loc]
+  (mapcat #(cons % (descendants %)) (children loc)))
+
+(defn descendants-and-self [loc]
+  (cons loc (descendants loc)))
+
+(defn loose-chain 
+  ([] list)
+  ([& sels] (reduce sel/chain (interpose descendants sels))))
+
+(defprotocol ^:private ToSelector
+  (as-sel [x]))
+
+(defn css [^String s]
+  (-> (org.apache.batik.css.parser.Parser.)
+    (.parseSelectors s)
+    as-sel))
+
+(defn sel
+  ([spec]
+    (sel spec :root))
+  ([spec relationship]
+    (if (sequential? spec) 
+      (first (reduce
+               (fn [[r relationship] step]
+                 (if (= :> step)
+                   [r (case relationship
+                        (:strict-root :root) :strict-root
+                        :children)]
+                   [(sel/chain r (sel step relationship)) :descendants]))
+              [list relationship] spec))
+      (sel/chain
+        (case relationship
+          :strict-root list
+          :root descendants-and-self
+          :children children
+          :descendants descendants)
+        (cond
+          (string? spec) (css spec)
+          (keyword? spec) (css (name spec))
+          :else spec)))))
+
+(defn- tag-is? [tag sel]
+  (or (nil? sel) (= tag sel)))
+
+(extend-protocol ToSelector
+  org.w3c.css.sac.SelectorList
+  (as-sel [sels] 
+    (let [sels (map #(as-sel (.item sels %)) (range (.getLength sels)))] 
+      (fn [loc]
+        (mapcat #(% loc) sels))))
+  org.w3c.css.sac.ElementSelector
+  (as-sel [sel]
+    (fn [loc]
+      (when (some-> (loc/node loc) :tag name (tag-is? (.getLocalName sel)))
+        (list loc))))
+  org.w3c.css.sac.DescendantSelector
+  (as-sel [sel]
+    (sel/chain (as-sel (.getAncestorSelector sel))
+      (case (.getSelectorType sel)
+        #=(eval org.w3c.css.sac.Selector/SAC_CHILD_SELECTOR)
+        children
+        #=(eval org.w3c.css.sac.Selector/SAC_DESCENDANT_SELECTOR)
+        descendants)
+      (as-sel (.getSimpleSelector sel))))
+  org.w3c.css.sac.ConditionalSelector
+  (as-sel [sel]
+    (sel/chain (as-sel (.getSimpleSelector sel)) (as-sel (.getCondition sel))))
+  org.w3c.css.sac.AttributeCondition
+  (as-sel [cond]
+    (let [pred (case (long (.getConditionType cond))
+                 #=(eval org.w3c.css.sac.Condition/SAC_ATTRIBUTE_CONDITION)
+                 (let [k (keyword (.getLocalName cond))]
+                   (if (.getSpecified cond) 
+                     (let [v (.getValue cond)] 
+                       #(some-> % loc/node :attrs (get k) (= v)))
+                     #(some-> % loc/node :attrs (get k))))
+                 #=(eval org.w3c.css.sac.Condition/SAC_CLASS_CONDITION)
+                 #(when-let [classes (some->> % loc/node :attrs :class (re-seq #"\S+") set)]
+                    (contains? classes (.getValue cond)))
+                 #_#_#=(eval org.w3c.css.sac.Condition/SAC_PSEUDO_CLASS_CONDITION)
+                   (case (.getValue cond)
+                     "first-of-type")
+                 #=(eval org.w3c.css.sac.Condition/SAC_ID_CONDITION)
+                 #(some-> % loc/node :attrs :id (= (.getValue cond))))]
+      (fn [loc] (when (pred loc) (list loc)))))
+  #_org.w3c.css.sac.CombinatorCondition
+  #_org.w3c.css.sac.NegativeCondition)
+
+
+(defn at [& selector+transformations]
+  (grounder/at* selector+transformations sel))
 
 ;; html-specific transformations
 (defn class
