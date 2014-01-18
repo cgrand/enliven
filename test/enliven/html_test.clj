7e437fd5ca93b83fb9cb643e9c5ad6bf6f233601
ckirkendall <ckirkendall@gmail.com>
2014-01-18 11:48:47 -0500

    added test for style transform

--- test/enliven/html_test.clj
+++ test/enliven/html_test.clj
@@ -2,7 +2,7 @@
   (:use clojure.test)
   (:require 
    [enliven.html :as h :refer [static-template content class
-                               append prepend]]
+                               append prepend style]]
    [enliven.html.jsoup :as jsoup]))
 
 
@@ -13,20 +13,26 @@
   (str "<html><head></head><body>" node-str "</body></html>"))
 
 (deftest transform-tests
-  (testing "content tranform"
+  (testing "content transform"
     (let [trans (static-template node :div (content :content))]
       (is (= (page-wrap "<div>test</div>")
              (trans {:content "test"})))))
-  (testing "class tranform"
+  (testing "class transform"
     (let [trans (static-template node :div (class :test :success))]
       (is (= (page-wrap "<div class='test'></div>")
              (trans {:success true})))))
-  (testing "append tranform"
+  (testing "append transform"
     (let [trans (static-template node2 :div (append :success))]
       (is (= (page-wrap "<div><span>t</span>test</div>")
              (trans {:success "test"})))))
-   (testing "prepend tranform"
+   (testing "prepend transform"
     (let [trans (static-template node2 :div (prepend :success))]
       (is (= (page-wrap "<div>test<span>t</span></div>")
-             (trans {:success "test"}))))))
+             (trans {:success "test"})))))
+   (testing "style transform"
+     (let [trans (static-template node2 :div (style :display :display
+                                                    :font-size :fs))]
+      (is (= (page-wrap "<div style='display:none;font-size:12px;'><span>t</span></div>")
+             (trans {:display "none"
+                     :fs "12px"}))))))
 
