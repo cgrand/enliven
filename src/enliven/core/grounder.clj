f391572ab9f27f3292b310ad0e954f34bfa2628a
Christophe Grand <christophe@cgrand.net>
2014-01-27 14:38:33 +0100

    Remove node-annotation of actions

--- src/enliven/core/grounder.clj
+++ src/enliven/core/grounder.clj
@@ -5,12 +5,10 @@
 
 ;; a transformation is a function from loc to seq of [loc action]
 ;; rules (as returned by ground-loc) are seq of [path action]
-;; actions are annotated with their nodes.
 (defn ground-loc [transformation loc]
   (for [[loc action] (transformation loc)]
-    [(path/canonical (loc/path loc)) (-> action
-                                       (action/update :args path/canonical)
-                                       (assoc :node (loc/node loc)))]))
+    [(path/canonical (loc/path loc))
+     (action/update action :args path/canonical)]))
 
 (defn ground [transformation node]
   (ground-loc transformation (loc/loc node)))
